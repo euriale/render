42753e674f4b02989bbc2500e5916db25f553490
unknown <mjlucena@.summus.es>
2014-01-29 11:16:58 +0100

    cambios en la cabecera

--- themes/js/codigo.js
+++ themes/js/codigo.js
@@ -7,7 +7,9 @@ Si es menor de 1 el fondo irá más deprisa que el frente.
 var parallax  = -2;
 
 $(document).ready(function () {
+	cabecerablanca();
 	$(document).scroll(function () {
+	cabeceranaranja();
 		s = $(document).scrollTop();
 		/* Efecto parallax */
 		$(document).scroll(function () {
@@ -19,26 +21,29 @@ $(document).ready(function () {
 
 	})
 	$( "#cabecera" ).mouseover(function() {
-		$(this).css("background", "#fff");
-		$(".drive").css("color", "#FF8C00");
-		$(".navigation li").css("color", "#000");
-		$(this).css("filter", "alpha(opacity=100)");
-		$(this).css("opacity", "1");
-		$(this).css("-moz-opacity", "1");
-		$(this).css("-webkit-opacity", "1");
-
-
+		cabecerablanca();
 	});
 	$( "#cabecera" ).mouseout(function() {
-		$(this).css("background", "#FF8C00");
-		$(".drive").css("color", "#fff");
-		$(this).css("filter", "alpha(opacity=70)");
-		$(".navigation li").css("color", "#fff");
-		$(this).css("opacity", "0.7");
-		$(this).css("-moz-opacity", "0.7");
-		$(this).css("-webkit-opacity", "0.7");
+		cabeceranaranja();
 	});
 });
 
+function cabecerablanca(){
+		$("#cabecera" ).css("background", "#fff");
+		$(".drive").css("color", "#FF8C00");
+		$(".navigation li").css("color", "#000");
+		$("#cabecera" ).css("filter", "alpha(opacity=100)");
+		$("#cabecera" ).css("opacity", "1");
+		$("#cabecera" ).css("-moz-opacity", "1");
+		$("#cabecera" ).css("-webkit-opacity", "1");
+}
 
-
+function cabeceranaranja(){
+		$("#cabecera" ).css("background", "#FF8C00");
+		$(".drive").css("color", "#fff");
+		$("#cabecera" ).css("filter", "alpha(opacity=70)");
+		$(".navigation li").css("color", "#fff");
+		$("#cabecera" ).css("opacity", "0.7");
+		$("#cabecera" ).css("-moz-opacity", "0.7");
+		$("#cabecera" ).css("-webkit-opacity", "0.7");
+}		
