243e384ea47652b3fd847ffacfc47b70e298eff0
Kalhel <javierfc@outlook.com>
2014-01-31 14:55:19 +0100

    Eliminado logos raros de FAQ y añadido nuevo temporal

--- themes/views/home/faqs.php
+++ themes/views/home/faqs.php
@@ -2,10 +2,10 @@
 
 		<div class="cajablanca"><div id="faq"></div> 
 		<div class="seccioncaja">
-		 <div class="secciones" id="logos3"><img src="themes/img/logos/faq.jpg" title="Contacto" alt="Contacto" width="80px"></div>
+		 <div class="secciones" id="logos3"><img src="themes/img/faq_small.png" title="Contacto" alt="Contacto" width="200px"></div>
 		 <div class="secciones">
 		  <h4> Preguntas Frencuentes</h4>
-		  <div class="abreintnaranja"></div><div class="txt16naranja"> Pregunta1 </div><div class="cierraintnaranja"></div>
+		  <div class="txt16naranja"> Pregunta1 </div>
 		  <div style="clear:both"></div>
 		  <div class="txt16negro"> Respuesta 1 </div>
 		 </div>
