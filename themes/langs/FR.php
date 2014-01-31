5a5a24db0bb21ce9adb8be8788146c6e40087108
Kalhel <javierfc@outlook.com>
2014-01-31 17:04:34 +0100

    Menu

--- themes/langs/FR.php
+++ themes/langs/FR.php
@@ -1,9 +1,9 @@
 <?php
 define ("GRANJA_RENDER_ONLINE", "Granja de Render Online_FR");
-define ("INICIO", "Home");
+define ("INICIO", "Accueil");
 define ("SERVICIOS_RENDER", "Render Service");
-define ("TARIFAS", "Princing");
-define ("CONTACTO", "Contact");
+define ("TARIFAS", "Prix");
+define ("CONTACTO", "Nous Contacter");
 define ("FAQ", "FAQ");
-define ("TUTORIAL", "Tutorial");
+define ("TUTORIAL", "Tutoriels");
 ?>
