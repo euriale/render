a877826783379963bc1d6a3280221b60e451bbd4
Kalhel <javierfc@outlook.com>
2014-02-04 09:25:31 +0100

    Corregido PNG a png del robot

--- themes/views/home/faqs.php
+++ themes/views/home/faqs.php
@@ -6,7 +6,7 @@
   <img src="themes/img/faq_small.png" title="Contacto" alt="Contacto" width="200px">
  </div>
  <div class="secciones">
-	<h4>Preguntas Frencuentes</h4>
+	<h4>Preguntas Frecuentes</h4>
 	<div class="txt16naranja"> Pregunta1 </div>
 	<div style="clear:both"></div>
 	<div class="txt16negro"> Respuesta 1 </div>
