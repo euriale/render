1dcb13f4a4bbc0fa4d2634e21a5c4a6fc6d84a44
Kalhel <javierfc@outlook.com>
2014-02-07 12:30:14 +0100

    Merge

--- themes/langs/EN.php
+++ themes/langs/EN.php
@@ -2,7 +2,7 @@
 define ("GRANJA_RENDER_ONLINE", "Online Render Farm");
 define ("INICIO", "Home");
 define ("SERVICIOS_RENDER", "Render Service");
-define ("TARIFAS", "Princing");
+define ("TARIFAS", "Pricing");
 define ("CONTACTO", "Contact");
 define ("FAQ", "FAQ");
 define ("TUTORIAL", "Tutorial");
