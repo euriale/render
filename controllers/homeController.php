891867bcf268252293b2654dd5aeca04393c2088
kalhel <javierfc@outlook.com>
2014-02-08 22:11:27 +0100

    merge

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -2,7 +2,7 @@
 //render('home/diapo1');
 render('home/inicio');
 render('home/servicios');
-//render('home/contacto');
+// render('home/contacto');
 render('home/tarifas');
 render('home/faqs');
 ?>
