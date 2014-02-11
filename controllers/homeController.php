14db72cab1c84206e8b2d093f3c0d6e772cf3a41
unknown <mjlucena1@gmail.com>
2014-02-11 07:31:34 +0100

    haciendo la nueva estructura

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,11 +1,11 @@
 <?php	
 render('home/inicio');
-
+render('home/ventajas');
 /*
 
-render('home/servicios');
+
 render('home/contacto');
-render('home/contacto');
+
 render('home/tarifas');
 render('home/faqs');
 */
--- themes/views/home/inicio.php
+++ themes/views/home/inicio.php
@@ -3,7 +3,19 @@
 	<h2><?php echo GRANJA_RENDER_ONLINE; ?></h2>
 
 	<h3>Ponemos a tu disposicion una granja de render compatible con la mayoría del software 3D del mercado</h3>
-</div> 		
+</div> 	
+
+<div class="primeracaja">
+	<div class="cajamini">
+		<h4>Usas Maya para tus Proyectos</h4>
+	</div>
+	<div class="cajamini">
+		<h4>Ventajas del Render en la nube</h4>
+	</div>
+	<div class="cajamini">
+		<h4>Usas otro Software para tus Proyectos </h4>
+	</div>
+</div>
 
 
 
