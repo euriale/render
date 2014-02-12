fbd14e7b0ec38c6659a76267c97b61c8c037d036
Kalhel <javierfc@outlook.com>
2014-02-12 16:11:42 +0100

    Textos Maya

--- themes/views/maya/inicio.php
+++ themes/views/maya/inicio.php
@@ -7,41 +7,27 @@
 <div id="descripcion"></div> 
 <div class="primeracaja">
 <div class="auxtexto">
-<div class="titulo">Aplicaciones</div>  
+<div class="titulo">Hemos creado una aplicación que te ayudará a simplificar los pasos del renderizado de tu proyecto. </div>  
 <div class="textocontenido">
-Trabajamos con <strong>Cinema 4D</strong>, <strong>3ds Max</strong>, <strong>SoftImage</strong>, <strong>Blender</strong>, <strong>LightWave</strong>, <strong>After Effects</strong> en sus distintas versiones, así como la mayoría de motores del mercado, <strong>Vray</strong>, <strong>Arnold</strong>, <strong>Maxwell</strong> y diversidad de plugins.  
-<br>
-Continuamente ampliamos el número de aplicaciones instaladas en nuestra máquinas, si usas otro software contacta con nosotros y nuestros técnicos adecuaran las máquinas a tu proyecto. 
-</div>
-<div class="titulo">Proceso </div>  
-<div class="textocontenido">
+Una vez te hayas registrado, podrás descargarte un plugin para Maya y un sincronizador. Desde tu proptio Maya podrás configurar los valores de tu render (setup): resolución, número de frames  motores, etc, y automáticamente nuestro sincronizador subirá el proyecto a nuestra granja. 
+Desde la aplicación vía web, podrás visualizar el resultado de cada tarea y  obtener una vista previa.</div>
 
-Una vez te pongas en contacto con nosotros, te pediremos unos sencillos datos para crear tu cuenta de usuario en nuestro sistema, y te enviaremos por correo electrónico los datos de de conexión de nuestro ftp, para que subas tu proyecto. Disponemos de velocidad de 500/50 Mbps para que la velocidad no sea un problema.  
-<br>
-Una vez dispongamos del proyecto comenzaremos el proceso de renderizado, y confome van terminando las tareas, verás los resultados en tiempo real en tu cuenta de nuestro ftp. 
-</div>
-<div class="titulo">Granja </div>  
+<div class="titulo">Tenemos las versiones, motores y plugins más habituales </div>  
 <div class="textocontenido">
-
-Información de la granja: nodos modelos etc
-<br>
-Nuestra infraestructura está diseñada por profesionales para el render de imágenes y animaciones, con lo que el uso de nuestras máquinas está totalmente optimizado para obtener unos resultados eficaces y sin incidentes.  
-</div>
-<div class="titulo">Precio </div>  
+Podrás lanzar trabajos de Maya 2014, 2013, 2012 y 2011. En cuanto a motores trabajamos con  Vray, MentalRay, Maya Softwre, Arnold, Maya Vector y Maxwell. Y los plugins Vue 11.5 y Realflow. Si usas otros motores o plugins contacta con nosotros.</div>
+<div class="titulo">Granja</div>
 <div class="textocontenido">
+[INFORMACION PENDIENTE]
+Nuestra infraestructura está diseñada por profesionales para el renderizado de imágenes y animaciones, con lo que el uso de nuestras máquinas está totalmente optimizado para obtener los resultados deseados.</div>
 
-El precio final de renderizado depende de tu proyecto, se puede realizar una estimación lanzando un render de un frame, sabiendo el número  total de frames, y el tipo de procesador donde se ha lanzado la prueba nos dará una estimación.
-<br>
- Si lo deseas, podemos realizar una estimación sin compromiso, una vez nos hayas subido tu proyecto a nuestro FTP. 
-</div>
-<div class="titulo">¿Tienes más dudas? </div>  
+<div class="titulo">Créditos</div>
 <div class="textocontenido">
-
-Visita nuestras preguntas frecuentes, o contacta con nosotros para resolverla. 
-<br>
-Si quieres darte de alta como cliente y que te hagamos un presupuesto a medida sin compromiso, rellena nuestro formulario y un técnico se pondrá en contacto contigo. Estaremos encantados de atenderte.
-</div> 
-</div>
+El Crédito es la unidad  es nuestra moneda de cambio del sistema. Consumirás créditos por los servicios que el sistema te ofrece
+A través de nuestra plataforma realizas la compra de créditos vía PayPal o a tarjeta  Visa. 
+El precio de un crédito es de un 1 €. 
+Aprovecha nuestras ofertas para obtener importantes descuentos. Los créditos que compres no caducarán nunca.</div>
+<div class="titulo">Costes</div>
+Los costes dependen de tu proyecto, se puede realizar una estimación lanzando un render de un frame, sabiendo el número  total de frames, y el tipo de procesador donde se ha lanzado la prueba nos dará una estimación.</div>
 </div>
 
 
