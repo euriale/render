182238af23cc46f0b3042ed8c018b717501742f0
MJ <mjlucena1@gmail.com>
2014-02-12 10:50:53 +0100

    caca

---
+++ controllers/no-mayaController.php
@@ -0,0 +1,16 @@
+<?php	
+render('no-maya/submenu');
+render('no-maya/inicio');
+render('no-maya/ventajas');
+/*
+render('no-maya/contacto');
+render('no-maya/tarifas');
+render('no-maya/faqs');
+*/
+?>
+
+
+
+
+
+
