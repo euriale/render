67e9d41cdc7468d24edb8a1774e2aaa5cd77181c
Kalhel <javierfc@outlook.com>
2014-02-12 15:49:57 +0100

    Inicio (Textos)

--- themes/views/home/inicio.php
+++ themes/views/home/inicio.php
@@ -8,21 +8,25 @@
 <div class="primeracaja">
 
 	<div class="cajamini">
-		<h4>Uso Maya para mis Proyectos</h4>
-		<div class="txt15"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</div>
-		<div class="imagencaja"><img src="themes/img/logos/logo-maya2.png" width="30%"></div>
+		<h4>Uso Maya para mis Proyectos</h4><br>
+		<div class="imagencaja"><img src="themes/img/teclado.jpg" width="60%"></div><br>
+		<div class="txt15">	Regístrate y accede a nuestra aplicación
+		<br>Si te das de alta hasta antes del 1 de Marzo te damos un <strong> 20% de descuento </strong>.</div>
+		
 		<div class="link"><a href="maya">Saber más</a></div>
 	</div>
 	<div class="cajamini">
-		<h4>Por qué renderizar con RenderDrive</h4>
-		<div class="txt15"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</div>
+		<h4>Por qué usar RenderDrive</h4>
+		<div class="txt15"> Conoce las ventajas de usar renderderizar en la nube.</div>
 		<div class="imagencaja"><img src="themes/img/teclado.jpg" width="60%"></div>
 		<div class="link"><a href="#ventajas" class="ancla">Saber más</a></div>
 		
 	</div>
 	<div class="cajamini">
 		<h4>Uso otro Software para mis Proyectos </h4>
-		<div class="txt15"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</div>
+		<div class="txt15"> Trabajamos con Maya, Cinema 4D, 3ds Max,SoftImage, Blender, LightWave, After Effects.<br>
+
+.</div>
 		<div class="imagencaja"><img src="themes/img/logos/016.jpg" width="60%"></div>
 		<div class="link"><a href="no-maya">Saber más</a></div>
 	</div>
