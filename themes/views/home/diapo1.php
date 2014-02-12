a9f7ebe304dc1d1adabdf7cd426bbebb44efadff
Kalhel <javierfc@outlook.com>
2014-02-12 17:36:48 +0100

    Imagenes pruebas

---
+++ themes/views/home/diapo1.php
@@ -0,0 +1,10 @@
+ <div class="" id="diapo1">
+		<!--<img src="themes/img/12.jpg" style="">
+		<img src="themes/img/WPTVG48X.png" style="width:100%;position:fixed;z-index:1000;">
+
+		<img src="themes/img/WPTVG48X.png" style="position:fixed;z-index:1000;">
+		<img src="themes/img/3.jpg" style="width:100%" >
+		<img src="themes/img/3d-gris.jpg" style="width:100%">
+		-->
+</div>
+<!-- <div class="" id="diapo2"></div>-->
--- themes/views/home/inicio.php
+++ themes/views/home/inicio.php
@@ -10,24 +10,26 @@
 	<div class="cajamini">
 		<h4>Uso Maya para mis Proyectos</h4><br>
 		<div class="imagencaja"><img src="themes/img/logos/maya.png" width="30%"></div><br>
-		<div class="txt15">	Regístrate y accede a nuestra aplicación
-		<br>Si te das de alta hasta antes del 1 de Marzo te damos un <strong> 20% de descuento </strong>.</div>
-		
+		<div class="txt15">	Regístrate ya y accede a nuestra aplicación
+		<br>Si te das de alta hasta antes del 1 de Marzo te regalamos un <strong> 20% de descuento </strong>.</div>
+		<br>
 		<div class="link"><a href="maya">Saber más</a></div>
 	</div>
 	<div class="cajamini">
-		<h4>Por qué usar RenderDrive</h4>
-		<div class="txt15"> Conoce las ventajas de usar renderderizar en la nube.</div>
+		<h4>Por qué usar RenderDrive</h4><br>
 		<div class="imagencaja"><img src="themes/img/cpdbyn.jpg" width="60%"></div>
+		<div class="txt15"> Conoce las ventajas de usar renderderizar en la nube.</div>
+		<br>
+	
 		<div class="link"><a href="#ventajas" class="ancla">Saber más</a></div>
 		
 	</div>
 	<div class="cajamini">
-		<h4>Uso otro Software para mis Proyectos </h4>
-		<div class="txt15"> Trabajamos con Maya, Cinema 4D, 3ds Max,SoftImage, Blender, LightWave, After Effects.<br>
-
-.</div>
+		<h4>Uso otro Software para mis Proyectos </h4><br>
 		<div class="imagencaja"><img src="themes/img/logos/016.jpg" width="60%"></div>
+		<div class="txt15"> Trabajamos con Maya, Cinema 4D, 3ds Max,SoftImage, Blender, LightWave, After Effects.
+</div>
+		<br>
 		<div class="link"><a href="no-maya">Saber más</a></div>
 	</div>
 </div>
