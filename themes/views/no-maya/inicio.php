521c6080ceb526815510fc19693b1dc17e2e04bb
Kalhel <javierfc@outlook.com>
2014-02-13 17:08:27 +0100

    No maya

--- themes/views/no-maya/inicio.php
+++ themes/views/no-maya/inicio.php
@@ -9,20 +9,21 @@
 <div class="auxtexto">
 <div class="titulo">Aplicaciones</div>  
 <div class="textocontenido">
+<img src="themes/img/nomaya/aplicaciones.jpg" align="right" width="30%">
 Trabajamos con <strong>Cinema 4D</strong>, <strong>3ds Max</strong>, <strong>SoftImage</strong>, <strong>Blender</strong>, <strong>LightWave</strong>, <strong>After Effects</strong> en sus distintas versiones, así como la mayoría de motores del mercado, <strong>Vray</strong>, <strong>Arnold</strong>, <strong>Maxwell</strong> y diversidad de plugins.  
 <br>
 Continuamente ampliamos el número de aplicaciones instaladas en nuestra máquinas, si usas otro software contacta con nosotros y nuestros técnicos adecuaran las máquinas a tu proyecto. 
 </div>
 <div class="titulo">Proceso </div>  
 <div class="textocontenido">
-
+<img src="themes/img/nomaya/proceso.jpg" align="left" width="30%">
 Una vez te pongas en contacto con nosotros, te pediremos unos sencillos datos para crear tu cuenta de usuario en nuestro sistema, y te enviaremos por correo electrónico los datos de de conexión de nuestro ftp, para que subas tu proyecto. Disponemos de velocidad de 500/50 Mbps para que la velocidad no sea un problema.  
 <br>
 Una vez dispongamos del proyecto comenzaremos el proceso de renderizado, y confome van terminando las tareas, verás los resultados en tiempo real en tu cuenta de nuestro ftp. 
 </div>
 <div class="titulo">Granja </div>  
 <div class="textocontenido">
-
+<img src="themes/img/nomaya/granja.jpg" align="right" width="30%">
 Información de la granja: nodos modelos etc
 <br>
 Nuestra infraestructura está diseñada por profesionales para el render de imágenes y animaciones, con lo que el uso de nuestras máquinas está totalmente optimizado para obtener unos resultados eficaces y sin incidentes.  
