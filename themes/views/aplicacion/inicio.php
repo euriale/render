948b9bcdf9d54fffdd958e63c0b96216531e0211
MJ <mjlucena1@gmail.com>
2014-02-21 14:11:21 +0100

    principal de aplicacion

--- themes/views/aplicacion/inicio.php
+++ themes/views/aplicacion/inicio.php
@@ -18,15 +18,16 @@
 			<p>Desde la aplicación vía web, podrás visualizar el resultado de cada tarea y  obtener una vista previa.</p>
 		</div>
 	</div>
-	<div class="cajaminidoble"><div class="foto"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div></div>
+	<div class="cajaminidoble"><img src="themes/img/25.jpg" width="350px" class="foto"></div>
 
 <!--segunda -->
 	<div style="width:100%;height:40px;"></div>
-	<div class="cajaminidoble"><div class="foto"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div></div>
+	<div class="cajaminidoble"><img src="themes/img/dna.jpg" height="350px" class="foto"></div>
 	<div class="cajaminidoble">	
-		<div class="tituloleft">Tenemos las versiones, motores y plugins más habituales </div>  
+		<div class="tituloleft">Versiones, motores y plugins más habituales </div>  
 		<div class="textocontenido">
-			Podrás lanzar trabajos de Maya 2014, 2013, 2012 y 2011. En cuanto a motores trabajamos con  Vray, MentalRay, Maya Softwre, Arnold, Maya Vector y Maxwell. Y los plugins Vue 11.5 y Realflow. Si usas otros motores o plugins contacta con nosotros.
+			<p>Podrás lanzar trabajos de <strong>Maya 2014</strong>, <strong>2013</strong>,<strong>2012</strong> y <strong>2011</strong>. En cuanto a motores trabajamos con  <strong>Vray</strong>, <strong>MentalRay</strong>, <strong>Maya Softwre</strong>, <strong>Arnold</strong>, <strong>Maya Vector</strong> y <strong>Maxwell</strong>. Y los plugins <strong>Vue 11.5</strong> y <strong>Realflow</strong></p>.
+			<p>Si usas otros motores o plugins contacta con nosotros.</p>
 		</div>
 	</div>
 
@@ -41,20 +42,20 @@
 			Nuestra infraestructura está diseñada por profesionales para el renderizado de imágenes y animaciones, con lo que el uso de nuestras máquinas está totalmente optimizado para obtener los resultados deseados.
 		</div>
 	</div>
-	<div class="cajaminidoble"><div class="foto"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div></div>
+	<div class="cajaminidoble"><img src="themes/img/estructura.jpg" align="right" width="350px" class="foto"></div>
 	
 
 	<!--cuarta -->
 	<div style="width:100%;height:40px;"></div>
-	<div class="cajaminidoble"><div class="foto"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div></div>
+	<div class="cajaminidoble"><img src="themes/img/robote.png" width="350px" class="foto">></div>
 	
 	<div class="cajaminidoble">	
 		<div class="tituloleft">Créditos</div>
 		<div class="textocontenido">
-			El Crédito es la unidad  es nuestra moneda de cambio del sistema. Consumirás créditos por los servicios que el sistema te ofrece
-			A través de nuestra plataforma realizas la compra de créditos vía PayPal o a tarjeta  Visa. 
-			El precio de un crédito es de un 1 €. 
-			Aprovecha nuestras ofertas para obtener importantes descuentos. Los créditos que compres no caducarán nunca.
+			<p>El Crédito es la unidad  es nuestra moneda de cambio del sistema. Consumirás créditos por los servicios que el sistema te ofrece.</p>
+			<p>A través de nuestra plataforma realizas la compra de créditos vía PayPal o a tarjeta  Visa. 
+			El precio de un crédito es de un 1€. </p>
+			<p>Aprovecha nuestras ofertas para obtener importantes descuentos. Los créditos que compres no caducarán nunca.</p>
 		</div>
 	</div>		
 	
@@ -65,9 +66,9 @@
 	<div class="cajaminidoble">	
 		<div class="tituloleft">Costes</div>
 		<div class="textocontenido">
-		Los costes dependen de tu proyecto, se puede realizar una estimación lanzando un render de un frame, sabiendo el número  total de frames, y el tipo de procesador donde se ha lanzado la prueba nos dará una estimación.
+		<p>Los costes dependen de tu proyecto, se puede realizar una estimación lanzando un render de un frame, sabiendo el número  total de frames, y el tipo de procesador donde se ha lanzado la prueba nos dará una estimación.</p>
 		</div>
 	</div>
-	<div class="cajaminidoble"><div class="foto"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div></div>
+	<div class="cajaminidoble"><img src="themes/img/build.jpg" width="350px" class="foto"></div>
 
 
