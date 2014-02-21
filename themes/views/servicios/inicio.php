53846810f0ed545619f561a904d9db26fb7346c4
MJ <mjlucena1@gmail.com>
2014-02-21 13:00:31 +0100

    estilos servicios

--- themes/views/servicios/inicio.php
+++ themes/views/servicios/inicio.php
@@ -1,62 +1,66 @@
 	
-<div class="slide2">
+<div class="slide3">
 	<h2><?php echo GRANJA_RENDER_ONLINE; ?></h2>
 
 	<h3>Ponemos a tu disposicion una granja de render compatible con la mayoría del software 3D del mercado</h3>
 </div> 	
 <div id="descripcion"></div> 
 <div class="primeracaja">
-<div class="auxtexto">
-<div class="titulo">Aplicaciones</div>  
-<div class="cajaminidoble">
-	<div class="textocontenido">
 
-		<p>Trabajamos con <strong>Cinema 4D</strong>, <strong>3ds Max</strong>, <strong>SoftImage</strong>, <strong>Blender</strong>, <strong>LightWave</strong>, <strong>After Effects</strong> en sus distintas versiones, así como la mayoría de motores del mercado, <strong>Vray</strong>, <strong>Arnold</strong>, <strong>Maxwell</strong> y diversidad de plugins. </p>
-		<p>
-		Continuamente ampliamos el número de aplicaciones instaladas en nuestra máquinas, si usas otro software contacta con nosotros y nuestros técnicos adecuaran las máquinas a tu proyecto. </p>
-	</div>
+<!--primera -->
+		 
+		<div class="cajaminidoble">
+		<div class="tituloleft">Aplicaciones</div> 
+			<div class="textocontenido">
+				
+				<p>Trabajamos con <strong>Cinema 4D</strong>, <strong>3ds Max</strong>, <strong>SoftImage</strong>, <strong>Blender</strong>, <strong>LightWave</strong>, <strong>After Effects</strong> en sus distintas versiones, así como la mayoría de motores del mercado, <strong>Vray</strong>, <strong>Arnold</strong>, <strong>Maxwell</strong> y diversidad de plugins. </p>
+				<p>
+				Continuamente ampliamos el número de aplicaciones instaladas en nuestra máquinas, si usas otro software contacta con nosotros y nuestros técnicos adecuaran las máquinas a tu proyecto. </p>
+			</div>
+		</div>
+		<div class="cajaminidoble"><br><br><div class="foto"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div></div>
+
+<!--segunda -->
+		<div style="width:100%"></div>
+		<div class="cajaminidoble"><div class="tituloleft">Proceso </div> <img src="themes/img/rd.jpg" align="right" width="350px" class="foto"></div>
+		<div class="cajaminidoble">
+			<div class="textocontenido">
+			<br><br>
+				<p>
+				Una vez te pongas en contacto con nosotros, te pediremos unos sencillos datos para crear tu cuenta de usuario en nuestro sistema, y te enviaremos por correo electrónico los datos de de conexión de nuestro ftp, para que subas tu proyecto. 
+				</p>
+				<p>Disponemos de velocidad de 500/50 Mbps para que la velocidad no sea un problema.  </p>
+				<p>
+				Una vez dispongamos del proyecto comenzaremos el proceso de renderizado, y confome van terminando las tareas, verás los resultados en tiempo real en tu cuenta de nuestro ftp. 
+				</p>
+			</div>
+		</div>
+
+<!--tercera -->
+		<div style="width:100%"></div>
+		<div class="cajaminidoble"> 
+		<div class="tituloleft">Granja </div>
+			<div class="textocontenido">
+				<p>Información de la granja: nodos modelos etc</p>
+				<p>
+				Nuestra infraestructura está diseñada por profesionales para el render de imágenes y animaciones, con lo que el uso de nuestras máquinas está totalmente optimizado para obtener unos resultados eficaces y sin incidentes.  </p>
+			</div>
+		</div>
+		<div class="cajaminidoble"><br>	<br><img src="themes/img/moto.jpg" align="right" width="350px" class="foto"></div>
+<!--cuarta -->
+		<div style="width:100%"></div>
+		<div class="cajaminidoble">	<div class="tituloleft">Precio </div>  <img src="themes/img/teclado.jpg" align="right" width="350px" class="foto"></div>
+		<div class="cajaminidoble">
+			<div class="textocontenido">
+			<br><br>
+			<p>
+			El precio final de renderizado depende de tu proyecto, se puede realizar una estimación lanzando un render de un frame, sabiendo el número  total de frames, y el tipo de procesador donde se ha lanzado la prueba nos dará una estimación.
+			</p>
+			<p>
+			 Si lo deseas, podemos realizar una estimación sin compromiso, una vez nos hayas subido tu proyecto a nuestro FTP. 
+			 </p>
+			</div>
+		</div>
+
 </div>
-<div class="cajaminidoble"><img src="themes/img/logos/logoscolormaya.png"><br><img src="themes/img/logos/logoscolor.png"></div>
-
-
-
-
-<div class="titulo">Proceso </div> 
-<div class="cajaminidoble">	<img src="themes/img/rd.jpg" align="right" width="350px"></div>
-<div class="cajaminidoble">
-	<div class="textocontenido">
-	<p>
-	Una vez te pongas en contacto con nosotros, te pediremos unos sencillos datos para crear tu cuenta de usuario en nuestro sistema, y te enviaremos por correo electrónico los datos de de conexión de nuestro ftp, para que subas tu proyecto. 
-	<p>Disponemos de velocidad de 500/50 Mbps para que la velocidad no sea un problema.  </p>
-	</p>
-	<p>
-	Una vez dispongamos del proyecto comenzaremos el proceso de renderizado, y confome van terminando las tareas, verás los resultados en tiempo real en tu cuenta de nuestro ftp. 
-	</p>
-	</div>
-
-</div>
-
-
-
-
-<div class="titulo">Granja </div>  
-<div class="textocontenido">
-<img src="themes/img/nomaya/granja.jpg" align="right" width="30%">
-Información de la granja: nodos modelos etc
-<br>
-Nuestra infraestructura está diseñada por profesionales para el render de imágenes y animaciones, con lo que el uso de nuestras máquinas está totalmente optimizado para obtener unos resultados eficaces y sin incidentes.  
-</div>
-<div class="titulo">Precio </div>  
-<div class="textocontenido">
-
-El precio final de renderizado depende de tu proyecto, se puede realizar una estimación lanzando un render de un frame, sabiendo el número  total de frames, y el tipo de procesador donde se ha lanzado la prueba nos dará una estimación.
-<br>
- Si lo deseas, podemos realizar una estimación sin compromiso, una vez nos hayas subido tu proyecto a nuestro FTP. 
-</div>
-
-
-</div>
-</div>
-
-
 
