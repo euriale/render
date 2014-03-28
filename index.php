551dbd094d766db1a2f8481eeabbf50aa772bc78
Kalhel <javierfc@outlook.com>
2014-03-28 11:54:34 +0100

    otro para google A

--- index.php
+++ index.php
@@ -46,7 +46,19 @@ else $GLOBALS['raiz']='/render/';
   ga('send', 'pageview');
 
 </script>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-xxxxx-y']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
 
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+ 
 </head>
 
 <body>
