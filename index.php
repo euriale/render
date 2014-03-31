7c809fc0e18406413d43569f387c888c10324b9d
MJ <mjlucena1@gmail.com>
2014-03-31 17:47:02 +0200

    validacion

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<!--<?php
-if(isset($_GET['r1']) && $_GET['r1']=='idioma') header ("Location: ../controllers/direct/idioma.php?r2=".$_GET['r2']);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+<title>Render Drive - Servicios de Render Online</title>
 
+
+<?php/*
+if(isset($_GET['r1']) && $_GET['r1']=='idioma') header ("Location: ../controllers/direct/idioma.php?r2=".$_GET['r2']);
 session_start();
 if (!isset($_SESSION['idioma'])){
     $idioma =substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);
@@ -12,20 +19,16 @@ require_once("themes/langs/".$idiomaaux.".php");
 
 error_reporting(E_ALL|E_STRICT);
 $site_path = realpath(dirname(__FILE__));
-define ('__SITE_PATH', $site_path);
+define ('__SITE_PATH', $site_path);*/
+?>
 
-?>-->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<META charset="ISO 8859-1">
-<title>Render Drive - Servicios de Render Online</title>
-<script src="themes/js/jquery.js"></script>
-<script language="javascript" src="themes/js/codigo.js" charset="ISO-8859-1"></script>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700|Roboto:300&amp;subset=latin-ext' rel='stylesheet'>
+<script src="themes/js/jquery.js" type="text/javascript" ></script>
+<script type="text/javascript" src="themes/js/codigo.js" charset="ISO-8859-1"></script>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700|Roboto:300&amp;subset=latin-ext' rel='stylesheet' />
 
 <?php 
+
 if ($_SERVER['HTTP_HOST']=="www.renderdrive.com") $raiz='http://www.renderdrive.com/';
 elseif ($_SERVER['HTTP_HOST']=="renderdrive.com") $raiz='http://renderdrive.com/';
 else $GLOBALS['raiz']='/render/';
@@ -39,10 +42,10 @@ else $GLOBALS['raiz']='/render/';
 
 
 <!--calculadora-->
- <link rel="stylesheet" href="themes/css/jquery-ui.css">
-<script src="themes/js/jquery-1.9.1.js"></script>
-<script src="themes/js/jquery-ui.js"></script>
-<script>
+ <link rel="stylesheet" href="themes/css/jquery-ui.css" />
+<script src="themes/js/jquery-1.9.1.js" type="text/javascript"></script>
+<script src="themes/js/jquery-ui.js" type="text/javascript"></script>
+<script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -60,7 +63,7 @@ else $GLOBALS['raiz']='/render/';
 
 <body>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script type="text/javascript">(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
@@ -68,8 +71,12 @@ else $GLOBALS['raiz']='/render/';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-<div id="respuesta"><div class="iconclose" style="float:right;"></div><div style="clear:both"></div><div id="respuestaaux"></div></div>
-				<div style="clear:both"></div>
+<div id="respuesta">
+	<div class="iconclose" style="float:right;"></div>
+	<div style="clear:both;"></div>
+	<div id="respuestaaux"></div>
+</div>
+<div style="clear:both;"></div>
 <?php
 	require_once(__SITE_PATH.'/controllers/Content.php');
 ?>
