75d4c9240b939e4fabe59980cf37e2473924ab22
MJ <mjlucena1@gmail.com>
2014-04-04 16:50:53 +0200

    cuadrado de google

--- themes/views/home/ventajas.php
+++ themes/views/home/ventajas.php
@@ -3,6 +3,26 @@
 <div class="titulo">Por qué trabajar con Render Drive</div>
 <div class="auxtexto2">
 <div class="textocontenido">
+<!-- Fragmento enriquecido  -->
+
+<div itemscope itemtype="http://data-vocabulary.org/Product">
+<img itemprop="image" src="themes/img/socialimg/logo150x150.jpg" align="left" class="fotomargin" />
+  <span itemprop="brand">Render Drive</span> te ofrece <span itemprop="name">servicios de render online</span>.<br />
+   <span itemprop="description">
+  Nuestra aplicación te ayudará a simplificar los pasos del renderizado de tu proyecto.
+  </span>
+  Pouedes probar nuestra
+  <span itemprop="category" content="Software > Software informático > Software de diseño y multimedia > Software de modelado en 3D">aplicación de render en la nube</span>.<br />
+  
+  <span itemprop="offerDetails" itemscope itemtype="http://data-vocabulary.org/Offer">
+   <meta itemprop="currency" content="EUR" /><span itemprop="availability" content="in_stock">Consulta nuestras promociones y obten importantes descuentos.</span><br />
+   Desde <span itemprop="price">2.90</span>Céntimos de Euro por GHzh. 
+    
+  </span>
+
+</div>
+
+<!-- Fin del fragmento enriquecido -->
 <p>
 Debido a la complejidad del software 3D es necesaria una gran cantidad de horas para renderizar un proyecto. Además, supone una gran inversión disponer del equipamiento destinado a esta tarea, gastos en licencias, costes de energía y refrigeración, mantenimiento, etc.
 </p>
@@ -12,7 +32,7 @@ Te indicamos a continuación las claves para que descubras las ventajas de trabaj
 </div></div>
 
 <!--primera -->
-		 
+		 <div style="width:100%;height:40px;"></div>
 		<div class="cajaminidoble">
 		<div class="tituloleft">Conseguimos reducir el tiempo de los procesos más de 100 veces</div> 
 			<div class="textocontenido">
@@ -24,7 +44,7 @@ Te indicamos a continuación las claves para que descubras las ventajas de trabaj
 				
 			</div>
 		</div>
-		<div class="cajaminidoble"><img src="themes/img/tiempo.png" align="right" width="450px" title="Render Drive" alt="Render Drive" /></div>
+		<div class="cajaminidoble"><img src="themes/img/tiempo.png" align="right" width="450px" title="Render Drive" alt="Render Drive" class="foto"/></div>
 
 <!--segunda -->
 		<div style="width:100%;height:40px;"></div>
